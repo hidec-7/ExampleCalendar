c2b564a02f3aaceef4637533446ed5459e6e8b28
hidec-7 <hiedematoaho0730127@gmail.com>
2021-06-19 23:28:09 +0900

    FSCalendar実装

--- ExampleCalender/ViewController.swift
+++ ExampleCalender/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import FSCalendar
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FSCalendarDelegate {
+    
+    private var calendar = FSCalendar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        calendar.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        calendar.frame = CGRect(x: 0,
+                                y: 50,
+                                width: view.frame.size.width,
+                                height: view.frame.size.width)
+        view.addSubview(calendar)
     }
 
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE MM-dd-YYYY"
+        let string = formatter.string(from: date)
+        print("\(string)")
+    }
 
 }
 
